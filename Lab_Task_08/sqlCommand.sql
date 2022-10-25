9c88aeef1d87f87eb8e0d94b9f3a06284cda174f
51AN <sianashsad@iut-dhaka.edu>
2022-10-25 12:00:19 +0600

    Lab 08 report completion

--- Lab_Task_08/sqlCommand.sql
+++ Lab_Task_08/sqlCommand.sql
@@ -1,11 +1,16 @@
 --1
-SELECT COUNT(t_id) as Transaction_45 FROM TRANSACTIONS WHERE a_id = '45';
+SELECT COUNT(t_id) as Transaction_45 
+FROM TRANSACTIONS WHERE a_id = '45';
 
 --2
-SELECT COUNT(type) as Totaldebit FROM TRANSACTIONS WHERE type = '1';
+SELECT COUNT(type) as Totaldebit 
+FROM TRANSACTIONS WHERE type = '1';
 
 --3
-SELECT a_id,amount,type,EXTRACT( YEAR FROM DTM) as year FROM TRANSACTIONS  GROUP BY a_id,amount,type,EXTRACT( YEAR FROM DTM) HAVING EXTRACT( YEAR FROM DTM) = '2020';
+SELECT a_id,amount,type,EXTRACT( YEAR FROM DTM) as year 
+FROM TRANSACTIONS  
+GROUP BY a_id,amount,type,EXTRACT( YEAR FROM DTM) 
+HAVING EXTRACT( YEAR FROM DTM) = '2020';
 
 --4
 --given solution.java query
